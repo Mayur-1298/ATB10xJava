60e0de1eef1b9377edb24da6835629c8c463b70a
Mayur <mayurbhalekar9965@gmail.com>
2025-01-24 19:02:51 +0530

    19th Jan table task

---
+++ src/Table.java
@@ -0,0 +1,16 @@
+public class Table {
+    public static void main(String[] args) {
+        int n = Integer.parseInt(args[0]);
+        System.out.printf("%d X 1 = %d",n,n*1).println();
+        System.out.printf("%d X 2 = %d",n,n*2).println();
+        System.out.printf("%d X 3 = %d",n,n*3).println();
+        System.out.printf("%d X 4 = %d",n,n*4).println();
+        System.out.printf("%d X 5 = %d",n,n*5).println();
+        System.out.printf("%d X 6 = %d",n,n*6).println();
+        System.out.printf("%d X 7 = %d",n,n*7).println();
+        System.out.printf("%d X 8 = %d",n,n*8).println();
+        System.out.printf("%d X %d = %d",n,n,n*n).println();
+        System.out.printf("%d X 10 = %d",n,n*10).println();
+
+    }
+}
