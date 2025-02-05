186fd70951c75d9ed5097a7e46121b78b2773fba
Mayur <mayurbhalekar9965@gmail.com>
2025-02-05 13:15:15 +0530

    25th Jan reverse_String program

--- src/Reverse_String.java
+++ src/Reverse_String.java
@@ -5,13 +5,16 @@ public class Reverse_String {
 
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a word");
-        String user = sc.nextLine();
+        String user = sc.nextLine().toLowerCase();
         StringBuilder reverse = new StringBuilder();
         for (int i =user.length()-1; i>=0; i--){
             reverse.append(user.charAt(i));
 
         }
-        System.out.println(reverse);
+        if(reverse.toString().equals(user)){
+            System.out.println("Given word is a palindrome "+ reverse);
+        }else
+        System.out.println("Not a palindrome "+ reverse);
         sc.close();
 
     }
