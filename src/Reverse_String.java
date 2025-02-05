8e97922fcb13ab5b079325eb0cf422ef8374809e
Mayur <mayurbhalekar9965@gmail.com>
2025-02-05 13:07:10 +0530

    25th Jan reverse_String program

--- src/Reverse_String.java
+++ src/Reverse_String.java
@@ -4,10 +4,11 @@ public class Reverse_String {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a word");
         String user = sc.nextLine();
-        String reverse = "";
+        StringBuilder reverse = new StringBuilder();
         for (int i =user.length()-1; i>=0; i--){
-            reverse = reverse + user.charAt(i);
+            reverse.append(user.charAt(i));
 
         }
         System.out.println(reverse);
