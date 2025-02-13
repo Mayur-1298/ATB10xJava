301747ba89bf4c21d77b153e6faa08932c04862c
Mayur <mayurbhalekar9965@gmail.com>
2025-02-14 01:32:24 +0530

    7th Feb Forloop

--- src/Forloop/Factorial.java
+++ src/Forloop/Factorial.java
@@ -4,10 +4,18 @@ public class Factorial {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a number");
-        int num=sc.nextInt();
-        int Factorialvalue=1;
+        long num=sc.nextInt();
+        long Factorialvalue=1;
         for(int i=1;i<=num;i++){
              Factorialvalue=Factorialvalue*i;
         }System.out.println("Factorial of given number"+" = "+(Factorialvalue));
+        System.out.println("\n"+"Factorial value without for loop"+" = "+ Factorialwithoutloop(num));
+
+    }
+
+    public static long Factorialwithoutloop(long num){
+        if(num==0||num==1){
+            return 1;
+        }else return num*Factorialwithoutloop(num-1);
     }
 }
